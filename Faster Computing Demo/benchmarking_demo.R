f05d4abc092e084ba126f2b27a11b423c6525f2f
RebeccaKelble <156012978+RebeccaKelble@users.noreply.github.com>
2024-02-26 10:42:59 -0800

    Create benchmarking_demo.R

---
+++ Faster Computing Demo/benchmarking_demo.R
@@ -0,0 +1,47 @@
+# Clear workspace and close graphics
+rm(list = ls())
+graphics.off()
+
+set.seed(1) # for reproducibility
+
+geom_growth_base <- function(N0 = 2,
+                            T = 999,
+                            lambda = 1.01,
+                            sigma = 0.2){
+        Nvals <- vector('numeric') # initiate a place to put the values
+        Nvals[1] <- N0
+    for (t in 1:T){
+        Nvals[t+1] <- Nvals[t]*(lambda*exp(rnorm(1,0,sigma)))
+    }
+    return(Nvals)
+}
+# Run the simulation
+out <- geom_growth_base()
+# Plot the results
+plot(0:999,
+out,
+xlab='Time',
+ylab='Population size',
+type='o')
+
+# Simplest benchmark
+# Default number of time-points
+start_time <- Sys.time()
+out <- geom_growth_base()
+end_time <- Sys.time()
+time_elapsed <- end_time - start_time
+print(time_elapsed)
+
+# Repeat simplest benchmark with longer time horizing
+# Repeat with greater number of time-points
+start_time2 <- Sys.time()
+out2 <- geom_growth_base(T = 9E5)
+end_time2 <- Sys.time()
+time_elapsed2 <- end_time2 - start_time2
+print(time_elapsed2)
+
+# Benchmark with system.time
+system.time(geom_growth_base(T = 9E5))
+
+# Benchmark with microbanchmark
+library(microbenchmark)
